b6a312229c5db7b25e60f9d78e3e86cdb8f6d0cf
Konstantin Kalinin <konstantin.kalinin@ispring.com>
2025-12-15 22:21:57 +0300

    Написал основную часть 10-ой лабы

--- lw10/src/main.cpp
+++ lw10/src/main.cpp
@@ -1,5 +1,27 @@
-#include <iostream>
+#include "client/client.h"
+#include "server/server.h"
 
-int main() {
-	return EXIT_SUCCESS;
+#include <iostream>
+#include <string>
+
+int main(const int argc, char** argv)
+{
+	if (argc < 2)
+	{
+		std::cerr << "Usage: " << argv[0] << " [server|client]" << std::endl;
+		return 1;
+	}
+	if (const std::string mode = argv[1]; mode == "server")
+	{
+		return RunServer();
+	}
+	else
+	{
+		if (mode == "client")
+		{
+			return RunClient(argc, argv);
+		}
+		std::cerr << "Unknown mode: " << mode << ". Use 'server' or 'client'." << std::endl;
+		return 1;
+	}
 }
